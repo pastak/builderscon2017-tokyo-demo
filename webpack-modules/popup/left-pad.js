211df90a979531698e138843f79f17308fc48a9b
pastak <pasta0915@gmail.com>
2017-08-03 04:34:22 +0900

    valueAsNumber

--- webpack-modules/popup/left-pad.js
+++ webpack-modules/popup/left-pad.js
@@ -9,14 +9,14 @@ document.getElementById("pad").addEventListener("click", (e) => {
     e.preventDefault();
 
     console.log("padding");
-    resultNode.value = leftPad(textNode.value, amountNode.valueAsNumber, withNode.value);
+    resultNode.value = leftPad(textNode.value, Number(amountNode.value), withNode.value);
 }, false);
 
 document.getElementById("pad-bg").addEventListener("click", (e) => {
     e.preventDefault();
     var sendingMessage = browser.runtime.sendMessage({
         text: textNode.value,
-        amount: amountNode.valueAsNumber,
+        amount: Number(amountNode.value),
         with: withNode.value
     });
     sendingMessage.then((result) => {
